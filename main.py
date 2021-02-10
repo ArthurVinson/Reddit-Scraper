9baf6665c40bbda2a688facdfd8e1495a911d8c6
ArthurVinson <69321121+ArthurVinson@users.noreply.github.com>
2021-02-09 23:35:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # Arthur Vinson
 # 2/8/2021
 
+# This scraper will do the following:
+# 1. Grab a collection of reddit comments from a selected subreddit using PRAW
+# 2. Perform sentiment analysis. 
+
+# Simple, no?
+
+# --- Grabbing Comments from Reddit ---
+# https://praw.readthedocs.io/en/latest/
+
+import praw
