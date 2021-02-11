0800feb7e9e2eb8899933b08429e60175ccd1e16
ArthurVinson <69321121+ArthurVinson@users.noreply.github.com>
2021-02-10 23:40:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,10 @@
 # https://praw.readthedocs.io/en/latest/
 
 import praw
+
+# create a reddit instance and provide it with client_id, client_secreat and a user_agent
+reddit = praw.Reddit(client_id='LK_PUjO05BqFSg', client_secret='FuE_G7UhRkvIsVNurMuo0VMO3J85ug', user_agent='scraper project')
+
+
+
+# future project will scrape (all of?) reddit for a keyword or key phrase and conduct sentiment analysis on comments it finds
