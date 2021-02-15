259144f11f0d544071788156de730c1b888aac5e
ArthurVinson <69321121+ArthurVinson@users.noreply.github.com>
2021-02-14 23:58:53 -0600

    clean up
    moved commented out to separate text file

--- main.py
+++ main.py
@@ -8,6 +8,11 @@
 
 # Simple, no?
 
+# Your task is to split posts into sentences, tag them with a PoS tagger that we will provide, gather some
+# feature information from each post, learn models, and use these to classify
+
+
+
 # --- Grabbing Comments from Reddit ---
 # https://praw.readthedocs.io/en/latest/
 
@@ -16,32 +21,11 @@ import praw
 # create a reddit instance and provide it with client_id, client_secreat and a user_agent
 reddit = praw.Reddit(client_id='LK_PUjO05BqFSg', client_secret='FuE_G7UhRkvIsVNurMuo0VMO3J85ug', user_agent='scraper project')
 
-# testing some capabilities of praw
-# - get the most recent 15 hot posts from a subreddit
+subr = 'Reformed' # put the name of the subreddit as a string variable for ease
 
-# find a relevant comment/post 
-# grab comments
-# parse the comments/clean the data for NLP
+subreddit = reddit.subreddit(subr)
 
+print(subreddit.display_name)
+print(subreddit.title)
+print(subreddit.description)
 
-sub = 'ffxiv'
-
-# categorize by 'hot' posts
-# hot_posts = reddit.subreddit(sub).hot(limit=15)
-# for post in hot_posts:
-#    print(post.title)
-
-# categorize by 'new' posts
-
-new_posts = reddit.subreddit(sub).new(limit=15)
-for post in new_posts:
-    print(post.title)
-
-# stuff to do - go over the reference document, look at a sentiment analysis tutorial
-
-# current project is to scrape reddit for [topic] and see what people feel about [topic]
-# current subreddit-topic pairs: reformed-baptists, [unknown]-artificial meat, rats-rats
-# * need to be able to scrape a subreddit, not just most recent but all
-# * grab comments (not just top level but all)
-
-# future project will scrape (all of) reddit ( for a keyword or key phrase and conduct sentiment analysis on comments it finds
