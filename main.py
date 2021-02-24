f2dc869971a2923af0c154aad06cc4be9650bc4c
ArthurVinson <69321121+ArthurVinson@users.noreply.github.com>
2021-02-23 23:58:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,17 @@
 # Your task is to split posts into sentences, tag them with a PoS tagger that we will provide, gather some
 # feature information from each post, learn models, and use these to classify
 
-
+# Comment added: 2/23/2021
+# 
+# Do these three things
+# 1. Scrape all comments from a given reddit thread
+# 2. Extract the top level comments
+# 3. Save to a csv file
 
 # --- Grabbing Comments from Reddit ---
 # https://praw.readthedocs.io/en/latest/
+
+# https://towardsdatascience.com/scraping-reddit-with-praw-76efc1d1e1d9
 
 import praw
 
@@ -22,6 +29,11 @@ import praw
 reddit = praw.Reddit(client_id='LK_PUjO05BqFSg', client_secret='FuE_G7UhRkvIsVNurMuo0VMO3J85ug', user_agent='scraper project')
 
 subr = 'Reformed' # put the name of the subreddit as a string variable for ease
+
+uniq_id = lqms6k # unique id for the thread
+
+submission = reddit.submission(id=uniq_id)
+
 
 subreddit = reddit.subreddit(subr)
 
