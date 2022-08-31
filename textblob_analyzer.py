7ed530e0ee23b33ebac5a019cf2dd74883eb3598
ArthurVinson <69321121+ArthurVinson@users.noreply.github.com>
2022-08-31 17:26:06 -0500

    second wind

---
+++ textblob_analyzer.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Sentiment Analyzer
+
+Created on Sun Aug 28 22:51:42 2022
+
+@author: Arthur Vinson
+
+Inspired by https://levelup.gitconnected.com/simple-nlp-in-python-2cb3243239d3
+
+"""
+
+from textblob import TextBlob
+
+# Input Text
+
+input_text = '''I am a life long Star Wars fan and this was the first time I came out disappointed. And I am not picky, I was mostly happy even with the last two movies, but this one is the worst Star Wars movie yet.'''
+
+# Creating a textblob object an assigning the sentiment property
+
+analysis = TextBlob(input_text).sentiment
+print(analysis)
+
--- textblob_tutorial.py
+++ textblob_tutorial.py
@@ -7,7 +7,7 @@ Created on Thu Feb 18 20:58:44 2021
 
 # Textblob Tutorial here
 
-from textblob import TextBlob
+# from textblob import TextBlob
 
 
 blob = TextBlob("Analytics Vidhya is a great platform to learn data science. \n It helps community through blogs, hackathons, discussions,etc.")
